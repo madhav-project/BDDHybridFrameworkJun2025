ebfd29cb7db919625eeac8921b5533f779117899
madhav <maddyp238@gmail.com>
2025-09-14 09:19:25 +0530

    Added the Before and After hooks & Initialize the starting browser code

--- src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
+++ src/test/java/com/qa/stepdefinations/EmployeeCRUD.java
@@ -1,15 +1,34 @@
 package com.qa.stepdefinations;
 
+import com.qa.base.Base;
+import com.qa.pages.LoginPage;
+import com.qa.util.CaptureScreenshot;
+import com.qa.util.WaitMethods;
+
 import cucumber.api.DataTable;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class EmployeeCRUD {
+public class EmployeeCRUD extends Base {
+
+	Scenario scenario;
+	LoginPage objLoginPage;
+
+	@Before
+	public void loginIntoApplication(Scenario scenario) {
+		this.scenario = scenario;
+	}
 
 	@Given("^Navigate to PIM after log in with Admin user$")
 	public void navigate_to_PIM_after_log_in_with_Admin_user() throws Throwable {
-		System.out.println("Selenium to be added");
+		scenario.write("Starting the Orange HRM application in browser");
+		driver = initializeWebDriver();
+		WaitMethods.staticWait(5000);
+		scenario.embed(CaptureScreenshot.captureImage(driver), "image/png");
 	}
 
 	@When("^I Add employee with  first name as \"([^\"]*)\" and mname as \"([^\"]*)\" and lName as \"([^\"]*)\"$")
@@ -38,6 +57,12 @@ public class EmployeeCRUD {
 	public void i_select_and_Delete_the_Updated_Employee_and_verify_employee_is_not_in_search_result()
 			throws Throwable {
 		System.out.println("Selenium to be added");
+	}
+
+	@After
+	public void closeApplication(Scenario scenario) {
+		scenario.write("closing the application");
+		closeBrowser();
 	}
 
 }
